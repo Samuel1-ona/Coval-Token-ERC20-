1d490c9ea731eafa16da4012ae960cf5456c768e
Samuel1-ona <samuonanikesamuel123@gmail.com>
2024-02-15 11:03:16 +0100

    initial commit

---
+++ contracts/Coval.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.19;
+
+contract Coval {
+
+    // State variables
+    uint256  tokenSupply;
+    string public symbol;
+    uint public decimals; 
+    string public tokenName;
+    address  owner;
+
+   
+}
